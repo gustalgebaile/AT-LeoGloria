e0cdde5e0dc1729d9fcc0c56140d6768072e4152
gustalgebaile <gualgebaile@gmail.com>
2025-09-19 00:15:40 -0300

    Nova Att 2

--- src/main/java/org/example/Exe5/PriorityQueueSort.java
+++ src/main/java/org/example/Exe5/PriorityQueueSort.java
@@ -7,18 +7,18 @@ public final class PriorityQueueSort {
     private PriorityQueueSort() {}
 
     public static int[] sort(int[] arr) {
-        if (arr == null || arr.length == 0) {    // Decisão 1
-            return arr;                          // Ramificação A
+        if (arr == null || arr.length == 0) {
+            return arr;
         }
 
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int num : arr) {                    // Loop 1
+        for (int num : arr) {
             pq.offer(num);
         }
 
         int i = 0;
-        while (!pq.isEmpty()) {                  // Loop 2
-            arr[i++] = pq.poll();               // Ramificação B
+        while (!pq.isEmpty()) {
+            arr[i++] = pq.poll();
         }
 
         return arr;
